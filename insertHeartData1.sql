1a84ea5dd0e336d0c415ec10bcade132bd66d228
Michael Malgeri <612867+mmalgeri@users.noreply.github.com>
2022-08-22 15:24:52 -0700

    Add files via upload

---
+++ insertHeartData1.sql
@@ -0,0 +1,164 @@
+INSERT INTO heartMonitors(
+    account_id, 
+    hardware_address, 
+    serial_number, 
+    info, 
+    last_reported
+)
+VALUES
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 190
+        }
+      }',
+    now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 80
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 70
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 90
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 110
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 120
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 130
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 140
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 150
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 160
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 170
+        }
+      }',
+      now()
+  ),
+  (
+    3, 
+    '02:51:b6:ba:1a:3f', 
+    '777', 
+    '{"info":{
+        "voltage_volts": 102.00,
+        "current_amps": 0.037,
+        "power_watts": 0,
+        "beatsPerMinute": 190
+        }
+      }',
+      now()
+  );
